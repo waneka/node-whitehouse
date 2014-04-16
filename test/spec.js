ac52cbd4d4811ff0ba6b81ed204fd9aad61845f3
Tyler <twwaneka@gmail.com>
2014-04-16 14:33:06 -0700

    tests for getSignatures
    - tests with correct number of arguments (2 or 3)
    - test with wrong number of args (0 or 4)

--- test/spec.js
+++ test/spec.js
@@ -4,6 +4,39 @@ var assert = require('assert')
   , wh = require('../lib/WhiteHouse')
   , whApi = wh.createWhiteHouse()
 
+describe('getSignatures()', function() {
+  describe('called with 2 arguments (petition & callback)', function() {
+    it('should call the function apiCall', function() {
+      var stub = sinon.stub(whApi, 'apiCall')
+
+      whApi.getSignatures('fakePetition', 'fakeCallback')
+      stub.called.should.be.true
+
+      whApi.apiCall.restore()
+    })
+  })
+  describe('called with three arguments (petition, params, & callback)', function() {
+    it('should call the function apiCall', function() {
+      var stub = sinon.stub(whApi, 'apiCall')
+
+      whApi.getSignatures('fakePetition', 'fakeParams', 'fakeCallback')
+      stub.called.should.be.true
+
+      whApi.apiCall.restore()
+    })
+  })
+  describe('called with the wrong number of arguments', function() {
+    it('should throw and error', function() {
+      var stub = sinon.stub(whApi, 'apiCall')
+
+      whApi.getSignatures.bind(null).should.throw()
+      whApi.getSignatures.bind(null, 'fakePetition', 'fakeParams', 'fakeCallback', 'extraArg').should.throw()
+
+      whApi.apiCall.restore()
+    })
+  })
+})
+
 describe('getPetitions()', function() {
   // beforeEach()
   describe('called with one argument (a callback)', function() {
